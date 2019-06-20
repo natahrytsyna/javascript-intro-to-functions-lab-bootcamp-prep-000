b9cf3898eaab8879a900be1a85240f7417d1af10
Nataliia Hrytsyna <nataliia.hrytsyna@gmail.com>
2019-06-20 10:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,29 +10,18 @@ function logShout(string) {
   console.log(shout('hello'))
 }
 
+function logWhisper(string) {
+  console.log(whisper('HELLO'))
+}
 
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+function sayHiToGrandma(string) {
+  if(string.toEqual('hello')) {
+    return "I can't hear you!"
+  }
+  if(string.toEqual('HELLO')) {
+    return "YES INDEED!"
+  }
+  if(string.toEqual("I love you, Grandma.")) {
+    return "I love you, too."
+  }
+}
